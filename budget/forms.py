79f88bacbbd5a02826204fe3848320bf4e9cb304
shootek <167685606+shootek@users.noreply.github.com>
2025-06-15 18:35:43 +0200

    Update forms.py

--- budget/forms.py
+++ budget/forms.py
@@ -1,16 +1,20 @@
 from django import forms
 from .models import Entry
+from captcha.fields import CaptchaField
 
 class EntryForm(forms.ModelForm):
+    captcha = CaptchaField()
+
     class Meta:
         model = Entry
-        fields = ['type', 'amount', 'category', 'date', 'description']
+        fields = ['type', 'amount', 'category', 'date', 'description', 'captcha']
         labels = {
-            'type': ('Typ'),
-            'amount': ('Kwota'),
-            'category': ('Kategoria'),
-            'date': ('Data'),
-            'description': ('Opis'),
+            'type': 'Typ',
+            'amount': 'Kwota',
+            'category': 'Kategoria',
+            'date': 'Data',
+            'description': 'Opis',
+            'captcha': 'Zabezpieczenie (Captcha)',
         }
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'}),
