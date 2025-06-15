2ac552ac936972b289df5fa9abf624dfcaabcfca
shootek <167685606+shootek@users.noreply.github.com>
2025-06-15 18:27:41 +0200

    Update urls.py

--- budget_project/urls.py
+++ budget_project/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from budget import views
+from reporting import views as reporting_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('add/', views.add_entry, name='add_entry'),
-
+    path('report/chart/', reporting_views.expenses_chart_view, name='chart'),
 
 ]
