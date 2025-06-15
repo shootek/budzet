e741c80837cd2932ee8cce22495017cc734d021f
shootek <167685606+shootek@users.noreply.github.com>
2025-06-15 18:34:17 +0200

    Update urls.py

--- budget_project/urls.py
+++ budget_project/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from budget import views
 from reporting import views as reporting_views
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('add/', views.add_entry, name='add_entry'),
     path('report/chart/', reporting_views.expenses_chart_view, name='chart'),
+    path('captcha/', include('captcha.urls')),
 
 ]
