7732787d6c98e070ddb884820605f083b83a0695
shootek <167685606+shootek@users.noreply.github.com>
2025-06-15 18:33:44 +0200

    Update settings.py

--- budget_project/settings.py
+++ budget_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +30,7 @@ ALLOWED_HOSTS = ['budzet.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'captcha',
     'budget',
     'django.contrib.admin',
     'django.contrib.auth',
