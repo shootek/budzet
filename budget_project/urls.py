4b5105921b912a7f67c4c96cb0d2ed711d376843
shootek <167685606+shootek@users.noreply.github.com>
2025-06-15 17:09:22 +0200

    Update urls.py

--- budget_project/urls.py
+++ budget_project/urls.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from django.urls import path
-from budget import views
+from budget import views  # Twoje istniejące widoki
+from reporting import views as reporting_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('add/', views.add_entry, name='add_entry'),
+
+    # 📊 Wykres wydatków
+    path('report/chart/', reporting_views.expenses_chart_view, name='chart'),
+
+    # 📄 Generowanie PDF
+    path('report/pdf/', reporting_views.generate_pdf_view, name='pdf'),
+
+    # 📧 Wysyłanie raportu e-mailem
+    path('report/email/', reporting_views.send_report_email, name='send_email'),
 ]
